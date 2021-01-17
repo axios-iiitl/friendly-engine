59d430d934133c7b5e54660ebf0b1067bc6aced1
narendraiiitl <lcs2019015@iiitl.ac.in>
2021-01-17 13:09:47 +0530

    fix

--- App.js
+++ App.js
@@ -4,10 +4,13 @@ const createError = require("http-errors");
 const morgan = require("morgan");
 const authRoutes = require("./Routes/Auth");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 require("dotenv").config();
 const { PORT } = process.env;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
--- Controller/auth.controller.js
+++ Controller/auth.controller.js
@@ -1,15 +1,24 @@
 const createError = require("http-errors");
 const User = require("../Model/User");
 const { signAccessToken } = require("../Helper/JWT_helper");
+const { OAuth2Client } = require("google-auth-library");
+const keys = require("../config/keys");
+const googleClient = new OAuth2Client(keys.clientID, keys.clientSecret);
+
 module.exports = {
-    googleauth: async (req, res, next) => {
-        try {
-          console.log(req.body);
-          const { googleId, email, name } = req.body;
-          User.findOne({ googleID: googleId }).then(async(currentuser) => {
+  googleauth: async (req, res, next) => {
+    try {
+      console.log(req.body);
+      const { idToken } = req.body;
+      googleClient
+        .verifyIdToken({ idToken, audience: keys.clientID })
+        .then((response) => {
+          const { email, googleId, name } = response.payload;
+          console.log(response.payload);
+          User.findOne({ googleID: googleId }).then(async (currentuser) => {
             if (currentuser) {
               const accessToken = await signAccessToken(currentuser.id);
-              res.send({accessToken});
+              res.send({ accessToken });
             } else {
               new User({
                 username: name,
@@ -17,16 +26,21 @@ module.exports = {
                 email: email,
               })
                 .save()
-                .then(async(newuser) => {
+                .then(async (newuser) => {
                   console.log("new user created" + newuser);
                   const accessToken = await signAccessToken(newuser.id);
-                  res.send({accessToken});
+                  res.send({ accessToken });
                 });
             }
           });
-        } catch (error) {
-          console.log(error.message);
-          next(createError(error));
-        }
-      },
-}
+        })
+        .catch((err) => {
+          next(createError.Conflict(err));
+        });
+    } catch (error) {
+      console.log("here ");
+      console.log(error.message);
+      next(createError(error));
+    }
+  },
+};
